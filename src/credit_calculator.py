ae0822fc511442a3d8fc5fbb982e91094cb309fc
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-28 19:03:27 +0300

    Added unsaved modification

--- src/credit_calculator.py
+++ src/credit_calculator.py
@@ -2,11 +2,11 @@ principal = int(input("Enter the credit principal:\n"))
 command = input("What do you want to calculate?\ntype 'm' - for count of months,\ntype 'p' - for monthly payment:")
 if command == "m":
     payment = int(input("Enter monthly payment:\n"))
-    period = principal / payment
+    period = principal // payment
     if period == 1:
         print("\nIt takes 1 month to repay the credit")
     else:
-        if isinstance(period, int):
+        if principal % payment == 0:
             print("\nIt takes {0} months to repay the credit".format(period))
         else:
             print("\nIt takes {0} months to repay the credit".format(principal // payment + 1))
