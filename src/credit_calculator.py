059282378e780371e6444f9013957c15387fdcd1
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-28 19:01:38 +0300

    Basic calculations

--- src/credit_calculator.py
+++ src/credit_calculator.py
@@ -1,11 +1,21 @@
-credit_principal = "Credit principal: 1000"
-final_output = "The credit has been repaid!"
-first_month = "Month 1: paid out 250"
-second_month = "Month 2: paid out 250"
-third_month = "Month 3: paid out 500"
-
-print(credit_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+principal = int(input("Enter the credit principal:\n"))
+command = input("What do you want to calculate?\ntype 'm' - for count of months,\ntype 'p' - for monthly payment:")
+if command == "m":
+    payment = int(input("Enter monthly payment:\n"))
+    period = principal / payment
+    if period == 1:
+        print("\nIt takes 1 month to repay the credit")
+    else:
+        if isinstance(period, int):
+            print("\nIt takes {0} months to repay the credit".format(period))
+        else:
+            print("\nIt takes {0} months to repay the credit".format(principal // payment + 1))
+elif command == "p":
+    count = int(input("Enter count of months:\n"))
+    payment = principal // count
+    if payment * count == principal:
+        print("\nYour monthly payment = {0}".format(payment))
+    else:
+        payment += 1 
+        last_paymment = principal - (count - 1) * payment
+        print("\nYour monthly payment = {0} with last month payment = {1}".format(payment, last_paymment))
